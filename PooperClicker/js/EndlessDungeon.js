63bc183776116d7ecf0fe4efc130ef30c95efe35
Cowwy <frankie.sc.law@hotmail.ca>
2019-09-29 09:26:41 -0400

    I think I got the Fix
    It was the storyBoard crash

--- PooperClicker/js/EndlessDungeon.js
+++ PooperClicker/js/EndlessDungeon.js
@@ -335,7 +335,7 @@ function devNoteControlSetup( ) {
 	\*/	
 		const message = {
 			1 : { "quote" : "I smell something funny on my hand...", 													"require" : { "Hand" :   0 } },
-			2 : { "quote" : "Poo is beginning to emerge - reported by fellow humans",		        					"require" : { "Hand" :   1 } },
+			2 : { "quote" : "Poo is beginning to emerge - reported by fellow humans",		        					"require" : { "Hand" :   0 } },
 			3 : { "quote" : "I feel the urge to clean this up but I smeared it everywhere.",   							"require" : { "Hand" :   2 } },
 			4 : { "quote" : "Rumor have been spreading that people is pooping at random places.",   					"require" : { "Hand" :  10 } },
 			5 : { "quote" : "My kid found a dead plant but it turns out that it is only covered in poo.",   			"require" : { "Hand" :  20 } },
