42ca37d00644f6826bb9cef7b670e395ba894182
Cowwy <frankie.sc.law@hotmail.ca>
2019-10-17 14:23:30 -0400

    Portfolio Update Again

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+
+const projectList = {
+	1 : { "projectTitle" : "Pooper Clicker (WIP)",
+		  "link"         : "http://cowwy.github.io/PooperClicker",
+		  "desc"         : "HTML | CSS | VanillaJS" },
+
+	2 : { "projectTitle" : "NoBul",
+		  "link"         : "http://cowwy.github.io/recreate/project1",
+		  "desc"         : "Static Web Page - HTML | SVG | CSS" },
+
+	3 : { "projectTitle" : "Simona Rich",
+		  "link"         : "http://cowwy.github.io/recreate/project2",
+		  "desc"         : "Single Responsive Page - HTML | CSS" },
+
+	4 : { "projectTitle" : "Juliana Rotich",
+		  "link"         : "http://cowwy.github.io/recreate/project3",
+		  "desc"         : "Full Responsive Site - HTML | CSS | VanillaJS" },
+
+	5 : { "projectTitle" : "Contact Form",
+		  "link"         : "http://cowwy.github.io/recreate/project4",
+		  "desc"         : "CSS Flex | JQuery" },
+
+	6 : { "projectTitle" : "CSS Grid - Pokedex",
+		  "link"         : "http://cowwy.github.io/project5",
+		  "desc"         : "CSS Grid" }
+}
