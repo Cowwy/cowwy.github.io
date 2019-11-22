1d9da4a34b84fce51149fdcde2a9c305d3d4d75b
Cowwy <frankie.sc.law@hotmail.ca>
2019-11-22 09:00:03 -0500

    Adjusting JS Code

--- sandbox/js/main.js
+++ sandbox/js/main.js
@@ -10,10 +10,19 @@ function initSectionHeight( id ) {
     const screenHeight  = window.innerHeight;
     const footerHeight  = $( ".footer-area" )[0].offsetHeight;    
     const sectionHeight = $( `#${id}Section` )[0].offsetHeight;
+    const padding = 160;
+
+    // const dom = document.getElementById( id + "Section" );
+
+    // console.log( $( `#${id}Section` )[0].offsetHeight );
+    // console.log( dom );
+    // console.log( dom.clientHeight );
+    // console.log( dom.offsetHeight );
+    // console.log( dom.scrollHeight );
 
     if( sectionHeight < (screenHeight - footerHeight) ) {
         const contact = document.getElementById( `${id}Section` );
-        contact.style.height = (screenHeight - footerHeight - sectionHeight ) + "px";
+        contact.style.height = (screenHeight - footerHeight - padding ) + "px";
     }
 }
 
