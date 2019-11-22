67b04679a933ce170058278b2a1ea4094d5d2c4c
Cowwy <frankie.sc.law@hotmail.ca>
2019-11-22 18:09:27 -0500

    d

--- sandbox/js/main.js
+++ sandbox/js/main.js
@@ -7,20 +7,32 @@ window.onload = function( ) {
 }
 
 function initControls( ) {
-    $( "#aboutMe, #project, #contact" ).click( ( e ) => {
-        const curId = e.currentTarget.id;   //aboutMe
+    $( "#aboutMe, #project, #contact, #aboutMe2, #project2, #contact2" ).click( ( e ) => {
+
+        const curId = ( e.currentTarget.id ).replace( "2", "" );   //aboutMe
         const prev  = state.activeSection;
 
         if( curId != prev ) {
             //previous active link becomes deactivated.
             //previous section becomes display none.
             $( `#${prev}Section`).toggleClass( "show" );
-            $( `#${prev}` ).toggleClass( "active" );
-            
             $( `#${curId}Section` ).toggleClass( "show" );
+
+            $( `#${prev}` ).toggleClass( "active" );
+            $( `#${prev}2` ).toggleClass( "active" );
+
             $( `#${curId}` ).toggleClass( "active" );
+            $( `#${curId}2` ).toggleClass( "active" );
+
+            $( `#burger-menu-list` ).addClass( "hide" );
+            $( "#burger-menu" ).removeClass( "lightOn" );
 
             state["activeSection"] = curId;
         }
     });
+
+    $( "#burger-menu" ).click( ( e ) => {
+        $( "#burger-menu" ).toggleClass( "lightOn" );
+        $( "#burger-menu-list" ).toggleClass( "hide" );
+    });    
 }
